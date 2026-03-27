3aa52698814f7b2aef2eebeab53aac2a6d279369
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-03-27 18:03:28 -0500

    refactor: return player stats as dict and include index names

--- data_pipeline/scrapers/players_info.py
+++ data_pipeline/scrapers/players_info.py
@@ -1,10 +1,8 @@
 import soccerdata as sd
-from pathlib import Path
 from scrapers.constants import league,season, export_path
 
 def export_players()->None:
-    path = export_path/"players.json"
     players = sd.Understat(leagues=league, seasons=season)
-    players_season = players.read_player_season_stats()
-    players_season.to_json(path_or_buf=path)
+    players_season = players.read_player_season_stats().reset_index()
+    return players_season.to_dict(orient="records")
     
