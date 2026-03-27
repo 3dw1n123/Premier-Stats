150479c067b7b8004241e9433428345dbf1f046f
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-03-27 18:01:58 -0500

    refactor: change league table output from JSON string to dict list

--- data_pipeline/scrapers/league_table.py
+++ data_pipeline/scrapers/league_table.py
@@ -1,13 +1,11 @@
 import soccerdata as sd
-from pathlib import Path
 from scrapers.constants import league, season,export_path
 
 def export_league_table()->None:
-    file_path = export_path/"league_table.json"
     sofascore = sd.Sofascore(leagues=league,seasons=season)
 
     table = sofascore.read_league_table()
 
-    table.to_json(path_or_buf=file_path, orient='records')
+    return table.to_dict(orient='records')
 
 
