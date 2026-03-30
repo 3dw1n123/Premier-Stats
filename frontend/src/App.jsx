33f7af026a5bf7959839e95f62521651239eaf45
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-03-30 15:54:50 -0500

    feat: orchestrate supabase data fetching in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
-import Standingtable from './components/static_table'
+import { useState, useEffect } from 'react'
+import Standingtable from './components/premier_table'
 import Stats_card from './components/stats_card'
 import top_scorers from './data/top_Scorers.json'
 import top_assist from './data/top_Assist.json'
-
-console.log(top_scorers)
+import { supabase } from './api/supabase_client'
+import Playertable from './components/player_table'
 
 function App() {
+
+const [table, settable] = useState([])
+const [players, setplayers] = useState([])
+
+useEffect (()=>{
+  const getData = async()=>{
+    try{
+      const{data,error} = await supabase.from('Positions').select('*').order('Pts', {ascending:false})
+      if (data) 
+      {settable(data)}
+    }catch(error){
+      console.log("error: ",error.message)
+    }
+
+  };
+
+  getData()
+  console.log(table)
+},[])
+
+
+useEffect (()=>{
+  const getData = async()=>{
+    try{
+      const{data,error} = await supabase.from('Players').select('*')
+      if (data) 
+      {setplayers(data)}
+    }catch(error){
+      console.log("error: ",error.message)
+    }
+
+  };
+
+  getData()
+  console.log(table)
+},[])
+
+
 
   return (
     <>
@@ -18,8 +56,9 @@ function App() {
       </header>
       <main className = "grid lg:grid-cols-[2fr_1fr] gap-6  ">
         <section className = "h-auto">
-          <Standingtable/>
-        </section>
+          {/*<Standingtable table={table}/>*/
+          <Playertable players ={players}/>
+}        </section>
         <aside className="flex flex-col gap-6 h-full p-8">
           <Stats_card title={"scorer"} list = {top_scorers} stat={"goals"}/>
           <Stats_card title = {"assistant"}  list = {top_assist} stat={"assists"}/>
