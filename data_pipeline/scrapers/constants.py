dec2056d04a5e32fc7dee0735c794d07f1cd90bc
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-06 22:26:16 -0500

    chore: remove unused legacy files

--- data_pipeline/scrapers/constants.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-league = 'ENG-Premier League'
-season = 2025
-export_path = Path("../frontend/src/data")
-
-
--- data_pipeline/scrapers/players_info.py
+++
@@ -1,8 +0,0 @@
-import soccerdata as sd
-from scrapers.constants import league,season, export_path
-
-def export_players()->None:
-    players = sd.Understat(leagues=league, seasons=season)
-    players_season = players.read_player_season_stats().reset_index()
-    return players_season.to_dict(orient="records")
-    
