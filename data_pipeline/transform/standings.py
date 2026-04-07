f724c2e0f1fbaeedb038871a00122afdad0b5a28
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-06 21:30:43 -0500

    feat: add data transforamtion pipeline for league standings

---
+++ data_pipeline/transform/standings.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def order_league_table(df:pd.DataFrame)->pd.DataFrame:
+    """
+    Sorts the league table alphabetically by team name.
+
+    Args:
+        df (pd.DataFrame): The raw league standings DataFrame.
+
+    Returns:
+        pd.DataFrame: A new DataFrame sorted by the team column.
+    """
+    df = df.copy()
+    return df.sort_values(by= "team")
+
+
+def process_league_table(league_table: pd.DataFrame)-> pd.DataFrame: 
+    """
+    Orchestrates the data transformation pipeline for the league standings.
+
+    Args:
+        league_table (pd.DataFrame): The raw league standings DataFrame extracted from the scraper.
+
+    Returns:
+        pd.DataFrame: The cleaned and transformed standings DataFrame.
+    """   
+    try:
+        df_clean =  (
+            league_table
+            .pipe(order_league_table)
+        )
+
+        return df_clean
+
+    except Exception as e:
+        print(f"Error processing standings data: {e}")
+        raise
