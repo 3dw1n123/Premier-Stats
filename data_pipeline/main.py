bc1d4925398fa31d450ccde89a40f43dec83a150
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-06 22:19:55 -0500

    refactor: update main orchestrator to use new transform and db modules

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -1,37 +1,72 @@
-from supabase import create_client, Client
-from dotenv import load_dotenv
-import os
+import pandas as pd
 from pathlib import Path
-from scrapers.league_table import export_league_table
-from scrapers.players_info import export_players
+#scrapers
+from scrapers.sofascore import get_sofascore_scraper,get_league_table
+from scrapers.understat import get_understat_scraper,get_player_stats
+from utils.constants import league, season
 
+#Transformers
+from transform.standings import process_league_table
+from transform.players import process_all_players
+from transform.mapper import process_mapping
 
-def setup_database()->Client:
-    load_dotenv()
-    url = os.getenv("SUPABASE_URL")
-    key = os.getenv("SUPABASE_KEY")
-    return create_client(url,key)
+#Databasee
+from db.engine import upsert_data, get_supabase_client
 
+def get_data() -> tuple[pd.DataFrame]:
+    """
+    Extracts raw data from all web sources.
 
-def upload_data(supabase: Client, table_name: str, data: list):
-    if not data:
-        print(f"No data to insert on {table_name}")
-        return
-    try:
-        response = (supabase.table(table_name).insert(data).execute())
-        return response
-    
-    except Exception as e:
-        print("error: {e}")
+    Returns:
+        tuple[pd.DataFrame]: A tuple containing the raw league table DataFrame and the raw player stats DataFrame.
+    """
+    sofascore_scraper = get_sofascore_scraper(league, season)
+    understat_scraper = get_understat_scraper(league, season)
+
+    league_table = get_league_table(sofascore_scraper)
+    player_stats = get_player_stats(understat_scraper)
+
+    return league_table, player_stats
+
+def cleaning_data(league_table:pd.DataFrame, player_stats:pd.DataFrame)->tuple[pd.DataFrame]:
+    """
+    Executes the transformation and cleaning pipelines for all datasets.
+
+    Args:
+        league_table (pd.DataFrame): The raw league standings DataFrame.
+        player_stats (pd.DataFrame): The raw player stats DataFrame.
+
+    Returns:
+        tuple[pd.DataFrame]: A tuple containing the cleaned league table, cleaned player stats, and the team mapping table.
+    """
+    clean_league_table = process_league_table(league_table)
+    clean_player_stats = process_all_players(player_stats)
+    clean_mapping = process_mapping(player_stats, league_table)
+
+    return clean_league_table, clean_player_stats, clean_mapping
+
 
 
 def main():
-    supabase = setup_database()
-    table = export_league_table()
-    players = export_players()
+    """
+    Main orchestrator for the ETL pipeline.
 
-    upload_data(supabase, "Positions",table)
-    upload_data(supabase, "Players",players)
+    Extracts raw data, transforms it, and loads the cleaned datasets into the Supabase database.
+    """
+    #Extract
+    league_table, player_stats = get_data()
+    
+    #Transform
+    clean_league_table, clean_player_stats, clean_mapping = cleaning_data(league_table, player_stats)
+
+    #Load
+    supabase = get_supabase_client()
+
+    upsert_data(supabase, "team_mapping", clean_mapping, "team_name")
+    upsert_data(supabase, "players",clean_player_stats,"player")
+    upsert_data(supabase, "positions", clean_league_table,"team")
+
+    print("ETL process completed successfully!")
 
 
 if __name__=="__main__":
