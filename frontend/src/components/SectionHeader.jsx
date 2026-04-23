6e28bb8743eb8a6a0fa4fd28f201e4011f5175ca
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-23 10:07:40 -0500

    feat: add reusable SectionHeader component

---
+++ frontend/src/components/SectionHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SectionHeader = ({subtitle, title, description}) => {
+  return (
+    <header className=" mb-10">
+        <div className=" mb-2 flex items-center gap-2">
+            <span className="text-xs font-bold tracking-widest text-secondary-premier">{subtitle}</span>
+        </div>
+        <h1 className="mb-4 -ml-1 text-5xl font-black tracking-tighter md:text-7xl">{title}</h1>
+        <p className="max-w-xl font-medium">{description}</p>
+
+    </header>
+  )
+}
+
+export default SectionHeader
