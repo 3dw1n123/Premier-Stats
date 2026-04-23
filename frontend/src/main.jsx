35695d0910319e3870038d6b489518cc6b88c77a
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-23 10:09:21 -0500

    feat: initialize and provide QueryClient

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import Players from './Players.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/players" element={<Players />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/players" element={<Players />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </QueryClientProvider>
+
 )
