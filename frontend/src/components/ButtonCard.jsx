56819fc019f71d97060985e45b0f53deae76f001
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-23 09:58:53 -0500

    style: unify box-shadow with global design

--- frontend/src/components/ButtonCard.jsx
+++ frontend/src/components/ButtonCard.jsx
@@ -1,7 +1,7 @@
 const ButtonCard = ({title,icon,bgColor}) => {
   return (
 
-    <div className ={`flex-1 ${bgColor} flex cursor-pointer flex-col justify-between rounded-xl p-4`}>
+    <div className ={`flex-1 ${bgColor} flex cursor-pointer flex-col justify-between rounded-xl p-4 shadow-lg`}>
                 <span className="text-4xl" data-icon="calendar_month">{icon}</span>
                 <span className="text-xs leading-tight font-bold tracking-widest text-white uppercase">{title}</span>
     </div>
