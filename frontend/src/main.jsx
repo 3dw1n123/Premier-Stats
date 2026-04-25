9f6888a65b9ff1f0325f2622a2a3de104994fe28
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-24 22:02:32 -0500

    feat: add dynamic route for player profile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Players from './Players.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import App from './App.jsx'
+import Players from './Players.jsx'
+import PlayerStats from './PlayerStats.jsx'
 
 const queryClient = new QueryClient()
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/players" element={<Players />} />
+          <Route path="/player/:id/:slug" element={<PlayerStats />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
