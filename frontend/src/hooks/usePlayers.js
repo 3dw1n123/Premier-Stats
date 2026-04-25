2c1c8714d348fb843e2f116fcc83d0aee319bc02
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-04-24 22:19:51 -0500

    chore: remove comments in usePlayers

--- frontend/src/hooks/usePlayers.js
+++ frontend/src/hooks/usePlayers.js
@@ -7,10 +7,8 @@ export const usePlayers = () =>{
   queryFn: getPlayers,
   initialPageParam: 0,
   getNextPageParam: (lastPage, allPages) =>{
-        // Si la última página vino vacía o incompleta, paramos
         if (lastPage.length < 8) return undefined;
         
-        // De lo contrario, devolvemos el número de la siguiente página
         return allPages.length;
   }
 })
