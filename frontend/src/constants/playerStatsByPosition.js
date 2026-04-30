17b716a826f72e8a015399571d69c8b181714ef1
javi-diaz47 <javiereduardo300@gmail.com>
2026-04-30 13:00:22 -0500

    feat: add player stats section by position

---
+++ frontend/src/constants/playerStatsByPosition.js
@@ -0,0 +1,133 @@
+export const PLAYER_STATS_SECTIONS_BY_POSITION = {
+  goalkeeper: [
+    {
+      section: "general",
+      title: "general",
+      isGeneral: true,
+      isBigger: true,
+    },
+    {
+      section: "playingTime",
+      title: "playing time",
+      isGeneral: true,
+      isBigger: false,
+    },
+    {
+      section: "discipline",
+      title: "discipline",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "goalkeeping",
+      title: "goalkeeping",
+      isGeneral: false,
+      isBigger: false,
+    },
+  ],
+
+  defender: [
+    {
+      section: "general",
+      title: "general",
+      isGeneral: true,
+      isBigger: true,
+    },
+    {
+      section: "playingTime",
+      title: "playing time",
+      isGeneral: true,
+      isBigger: false,
+    },
+    {
+      section: "discipline",
+      title: "discipline",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "defense",
+      title: "defense",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "teamImpact",
+      title: "team impact",
+      isGeneral: false,
+      isBigger: false,
+    },
+  ],
+
+  midfiler: [
+    {
+      section: "general",
+      title: "general",
+      isGeneral: true,
+      isBigger: true,
+    },
+    {
+      section: "playingTime",
+      title: "playing time",
+      isGeneral: true,
+      isBigger: false,
+    },
+    {
+      section: "attack",
+      title: "attack",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "defense",
+      title: "defense",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "teamImpact",
+      title: "team impact",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "discipline",
+      title: "discipline",
+      isGeneral: false,
+      isBigger: false,
+    },
+  ],
+
+  forward: [
+    {
+      section: "general",
+      title: "general",
+      isGeneral: true,
+      isBigger: true,
+    },
+    {
+      section: "playingTime",
+      title: "playing time",
+      isGeneral: true,
+      isBigger: false,
+    },
+    {
+      section: "attack",
+      title: "attack",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "penalties",
+      title: "penalties",
+      isGeneral: false,
+      isBigger: false,
+    },
+    {
+      section: "discipline",
+      title: "discipline",
+      isGeneral: false,
+      isBigger: false,
+    },
+  ],
+}
