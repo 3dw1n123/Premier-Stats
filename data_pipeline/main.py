d545d66c25bcb11b8e27575c30fa0cf0553498fc
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-05-04 17:49:35 -0500

    refactor: update ETL orchestration for FBref integration

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -1,72 +1,89 @@
 import pandas as pd
-from pathlib import Path
 #scrapers
 from scrapers.sofascore import get_sofascore_scraper,get_league_table
 from scrapers.understat import get_understat_scraper,get_player_stats
 from utils.constants import league, season
+from scrapers.fbref import get_player_general_stats, get_fbref_scraper,get_player_playing_time_stats, get_player_keeper_stats,get_player_misc_stats,get_player_shooting_stats
 
 #Transformers
 from transform.standings import process_league_table
 from transform.players import process_all_players
-from transform.mapper import process_mapping
 
 #Databasee
 from db.engine import upsert_data, get_supabase_client
 
-def get_data() -> tuple[pd.DataFrame]:
+def get_data() -> tuple[pd.DataFrame,list[pd.DataFrame],pd.DataFrame]:
     """
-    Extracts raw data from all web sources.
+    Extracts raw data from from SofaScore, Understat, and Fbref web sources.
 
     Returns:
-        tuple[pd.DataFrame]: A tuple containing the raw league table DataFrame and the raw player stats DataFrame.
+        tuple[pd.DataFrame]: tuple[pd.DataFrame, list[pd.DataFrame], pd.DataFrame]: A tuple containing:
+            - league_table (pd.DataFrame): The raw league standings DataFrame.
+            - dfs (list[pd.DataFrame]): A list of five raw Fbref player stat DataFrames.
+            - player_understat (pd.DataFrame): The raw Understat player stats DataFrame.
     """
     sofascore_scraper = get_sofascore_scraper(league, season)
     understat_scraper = get_understat_scraper(league, season)
+    fbref_scraper = get_fbref_scraper(league,season)
 
     league_table = get_league_table(sofascore_scraper)
-    player_stats = get_player_stats(understat_scraper)
+    player_stats = get_player_general_stats(fbref_scraper)
+    playing_time = get_player_playing_time_stats(fbref_scraper)
+    shooting_stats = get_player_shooting_stats(fbref_scraper)
+    gk_stats = get_player_keeper_stats(fbref_scraper)
+    misc_stats = get_player_misc_stats(fbref_scraper) 
+    player_understat = get_player_stats(understat_scraper)
 
-    return league_table, player_stats
+    dfs = [player_stats,playing_time,shooting_stats,misc_stats,gk_stats]
 
-def cleaning_data(league_table:pd.DataFrame, player_stats:pd.DataFrame)->tuple[pd.DataFrame]:
+
+    return league_table, dfs,player_understat
+
+def cleaning_data(league_table: pd.DataFrame, dfs: list[pd.DataFrame],understat: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Executes the transformation and cleaning pipelines for all datasets.
 
     Args:
         league_table (pd.DataFrame): The raw league standings DataFrame.
-        player_stats (pd.DataFrame): The raw player stats DataFrame.
+        dfs (list[pd.DataFrame]): A list of raw player statistics DataFrames from Fbref.
+        understat (pd.DataFrame): The raw player statistics DataFrame from Understat.
 
     Returns:
-        tuple[pd.DataFrame]: A tuple containing the cleaned league table, cleaned player stats, and the team mapping table.
+            tuple[pd.DataFrame, pd.DataFrame]: A tuple containing:
+            - clean_league_table (pd.DataFrame): The processed league table DataFrame.
+            - clean_player_stats (pd.DataFrame): The fully cleaned and merged player stats DataFrame.
     """
+
     clean_league_table = process_league_table(league_table)
-    clean_player_stats = process_all_players(player_stats)
-    clean_mapping = process_mapping(player_stats, league_table)
+    clean_player_stats = process_all_players(dfs,understat)
 
-    return clean_league_table, clean_player_stats, clean_mapping
-
+    return clean_league_table, clean_player_stats
 
 
-def main():
+
+def main() -> None:
     """
     Main orchestrator for the ETL pipeline.
 
-    Extracts raw data, transforms it, and loads the cleaned datasets into the Supabase database.
+    This function executes the complete end-to-end data pipeline:
+    1. Extract: Retrieves raw data from web sources (Fbref, Understat, SofaScore).
+    2. Transform: Cleans, merges, and standardizes the datasets.
+    3. Load: Upserts the fully processed data into the Supabase database.
     """
     #Extract
-    league_table, player_stats = get_data()
+    league_table, dfs,understat = get_data()
     
     #Transform
-    clean_league_table, clean_player_stats, clean_mapping = cleaning_data(league_table, player_stats)
+    clean_league_table, clean_player_stats = cleaning_data(league_table, dfs, understat)
+
 
     #Load
     supabase = get_supabase_client()
-
-    upsert_data(supabase, "team_mapping", clean_mapping, "team_name")
-    upsert_data(supabase, "players",clean_player_stats,"player")
-    upsert_data(supabase, "positions", clean_league_table,"team")
-
+    upsert_data(supabase, "players_stats", clean_player_stats, "player_name")
+    upsert_data(supabase, "positions", clean_league_table, "team")
+    
     print("ETL process completed successfully!")
+
 
 
 if __name__=="__main__":
