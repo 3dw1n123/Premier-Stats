a72c9cc795a038848aa7c942c83088a677f22dd9
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-05-06 12:34:41 -0500

    chore: disable the Sofascore scraper to the test the ETL process in Github Actions

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -22,11 +22,11 @@ def get_data() -> tuple[pd.DataFrame,list[pd.DataFrame],pd.DataFrame]:
             - dfs (list[pd.DataFrame]): A list of five raw Fbref player stat DataFrames.
             - player_understat (pd.DataFrame): The raw Understat player stats DataFrame.
     """
-    sofascore_scraper = get_sofascore_scraper(league, season)
+    #sofascore_scraper = get_sofascore_scraper(league, season)
     understat_scraper = get_understat_scraper(league, season)
     fbref_scraper = get_fbref_scraper(league,season)
 
-    league_table = get_league_table(sofascore_scraper)
+    #league_table = get_league_table(sofascore_scraper)
     player_stats = get_player_general_stats(fbref_scraper)
     playing_time = get_player_playing_time_stats(fbref_scraper)
     shooting_stats = get_player_shooting_stats(fbref_scraper)
@@ -37,9 +37,9 @@ def get_data() -> tuple[pd.DataFrame,list[pd.DataFrame],pd.DataFrame]:
     dfs = [player_stats,playing_time,shooting_stats,misc_stats,gk_stats]
 
 
-    return league_table, dfs,player_understat
+    return dfs,player_understat
 
-def cleaning_data(league_table: pd.DataFrame, dfs: list[pd.DataFrame],understat: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+def cleaning_data(dfs: list[pd.DataFrame],understat: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Executes the transformation and cleaning pipelines for all datasets.
 
@@ -54,10 +54,10 @@ def cleaning_data(league_table: pd.DataFrame, dfs: list[pd.DataFrame],understat:
             - clean_player_stats (pd.DataFrame): The fully cleaned and merged player stats DataFrame.
     """
 
-    clean_league_table = process_league_table(league_table)
+    #clean_league_table = process_league_table(league_table)
     clean_player_stats = process_all_players(dfs,understat)
 
-    return clean_league_table, clean_player_stats
+    return clean_player_stats
 
 
 
@@ -71,16 +71,16 @@ def main() -> None:
     3. Load: Upserts the fully processed data into the Supabase database.
     """
     #Extract
-    league_table, dfs,understat = get_data()
+    dfs,understat = get_data()
     
     #Transform
-    clean_league_table, clean_player_stats = cleaning_data(league_table, dfs, understat)
+    clean_player_stats = cleaning_data(dfs, understat)
 
 
     #Load
     supabase = get_supabase_client()
     upsert_data(supabase, "players_stats", clean_player_stats, "player_name")
-    upsert_data(supabase, "positions", clean_league_table, "team")
+    #upsert_data(supabase, "positions", clean_league_table, "team")
     
     print("ETL process completed successfully!")
 
