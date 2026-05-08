3e4461c2f98fa3b615e8c81674afcc5f4f6c480a
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-05-08 17:19:08 -0500

    feat: update usePlayers query to accept filter parameters

--- frontend/src/hooks/usePlayers.js
+++ frontend/src/hooks/usePlayers.js
@@ -1,15 +1,16 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getPlayers } from "../api/services";
 
-export const usePlayers = (search = "") =>{
+export const usePlayers = (filters) =>{
   return useInfiniteQuery({
-  queryKey: ["players", search],
-  queryFn: ({pageParam = 0}) => getPlayers({pageParam, search}),
+  queryKey: ["players", filters],
+  queryFn: ({pageParam = 0}) => getPlayers({pageParam, ...filters}),
   initialPageParam: 0,
   getNextPageParam: (lastPage, allPages) =>{
         if (lastPage.length < 8) return undefined;
         
         return allPages.length;
-  }
+  },
+  keepPreviousData: true
 })
 }
