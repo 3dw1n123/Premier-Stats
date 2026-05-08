c214e733bc3a93c346b23ecb59b2238874409ff8
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-05-08 17:18:09 -0500

    feat: implement sort by columns

--- frontend/src/Players.jsx
+++ frontend/src/Players.jsx
@@ -13,7 +13,22 @@ function Players() {
   const [search, setSearch] = useState("")
   const debounced = useDebounce(search)
 
-const {data, fetchNextPage,hasNextPage, isFetchingNextPage} = usePlayers(debounced);
+  const [sortConfig, setSortConfig] = useState({ column: null, order: "desc" });
+
+  const handleSort = (column) => {
+    setSortConfig((prevConfig) => {
+      if (prevConfig.column === column && prevConfig.order === "desc") {
+        return { column, order: "asc" };
+      }
+      return { column, order: "desc" };
+    });
+  };
+
+const {data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching} = usePlayers({
+    search: debounced,
+    sortBy: sortConfig.column,
+    sortOrder: sortConfig.order
+  });
 const allPlayers = data?.pages.flat() ?? [];
 
 const { ref, inView } = useInView({
@@ -34,7 +49,7 @@ const { ref, inView } = useInView({
                   <main className="w-full flex-1 p-6">
                     <SectionHeader subtitle={"DAILY UPDATES"} title={"PLAYERS STATISTICS"} description={"Premier League player statistics for the 2025/2026 season"}/>
 
-                    <label className="flex flex-col mb-6 text-[11px] font-extrabold uppercase tracking-widest text-secondary-premier">
+                      <label className="flex flex-col mb-6 text-[11px] font-extrabold uppercase tracking-widest text-secondary-premier">
                       Search
 
                       <div className="w-72 flex items-center gap-2 rounded-xl border-2 border-primary-premier px-4 py-2 shadow-sm tracking-normal">
@@ -47,7 +62,7 @@ const { ref, inView } = useInView({
                         />
                         <FaSearch className="shrink-0 w-5 h-5 text-primary-premier" />
                       </div>
-                    </label>
+                    </label>                   
 
                     <div className="max-w-[1440px] w-full">
                       <section className=" rounded-xl overflow-x-auto overflow-hidden shadow-lg w-full ">
@@ -55,6 +70,10 @@ const { ref, inView } = useInView({
                                           observerRef={ref}
                                           isFetchingNextPage={isFetchingNextPage}
                                           hasNextPage={hasNextPage}
+                                          onSort={handleSort}
+                                          sortBy={sortConfig.column}
+                                          sortOrder={sortConfig.order}
+                                          isFetching={isFetching}
                                           />
                       </section>
 
