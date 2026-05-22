0533b8e0a55662085a665bdcf164090cfb03abe6
3dw1n123 <77713206+3dw1n123@users.noreply.github.com>
2026-05-22 17:02:11 -0500

    refactor: consolidate card layout and update color scheme

--- frontend/src/components/PlayerProfileCard.jsx
+++ frontend/src/components/PlayerProfileCard.jsx
@@ -1,32 +1,31 @@
-const colorStyles = {
-  yellow: "border-l-4 border-yellow-400 bg-yellow-50 p-3 rounded-lg",
-  red: "border-l-4 border-red-500 bg-red-50 p-3 rounded-lg",
-};
-
 const getPerformanceColor = (stat) =>{
-    if(!stat.evaluable) return "";
+    if(!stat.evaluable) return "text-primary-premier";
     if(stat.value>0) return "text-green-500";
     if((stat.value)<0) return "text-red-500";
-    return "text-gray-500";
+    return "text-primary-premier";
 };
-
-const PlayerProfileCard = ({title,stats,isGeneral,isBigger}) => {
+const PlayerProfileCard = ({title,stats}) => {
   return (
-    isGeneral?
-    <section className={`rounded-xl border-t-4  p-6 shadow-lg  ${isBigger?`lg:col-span-2`:``}`}>
+    <section className={`rounded-xl border-t-4 border-primary-premier  p-6 shadow-lg }`}>
         <h2 className="mb-4 border-b pb-2  font-black tracking-widest text-secondary-premier uppercase">{title}</h2>    
         <div className="space-y-4">
             {stats.map((stat,index)=>(
-                <div className={`flex items-center justify-between ${colorStyles[stat.color]}`} key={index} title={stat.desc}>
-                    <span className="text-sm font-bold first-letter:uppercase">{stat.label.replaceAll("_", " ")}</span>
-                    <span className="text-2xl font-black italic">{stat.value}</span>
+                <div className={`flex items-center justify-between border-b border-gray-100 pb-3`} key={index}>
+                    <span className="text-gray-700 font-bold cursor-pointer" title={stat.desc}>{stat.label}</span>
+                    <div className="flex gap-1 font-black">
+                        <span className={`italic ${getPerformanceColor(stat)}`}>{stat.value}</span>
+                        <span className={`${stat.pct?``:`hidden`}`}>%</span>
+                    </div>
                 </div>
             ))}
         </div>
 
     </section>
-    
-    :
+  )}
+
+    {/*isGeneral?*/}
+
+    {/*:
 
     <section className="rounded-xl border-t-4 p-6 shadow-lg lg:col-span-3">
           <h2 className="mb-4 border-b pb-2 font-black tracking-widest text-secondary-premier uppercase">{title}</h2>
@@ -42,7 +41,6 @@ const PlayerProfileCard = ({title,stats,isGeneral,isBigger}) => {
     </section>
     
 
-  )
-}
+  )*/}
 
 export default PlayerProfileCard
